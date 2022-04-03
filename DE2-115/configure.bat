47e6860db6340225d3e6f915e1f9e6e44a1acf91
pgate1 <pgate.one@gmail.com>
2022-04-03 09:08:46 +0900

    Add files via upload

--- DE2-115/configure.bat
+++ DE2-115/configure.bat
@@ -2,9 +2,7 @@
 
 set PATH=%QUARTUS_ROOTDIR%\bin64;%QUARTUS_ROOTDIR%\bin32;%QUARTUS_ROOTDIR%\bin;%PATH%
 
-quartus_pgm.exe -c "USB-Blaster" -m JTAG -o p;PlayStation.sof
+quartus_pgm.exe -c "USB-Blaster" -m JTAG -o p;output_files\PlayStation.sof
 
 echo Press any key to finish.
 pause > nul
-
-exit
